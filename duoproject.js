c2445bf83a10e0f8db23a90a5d4cdeb20e680ef7
unknown <farah.kh@hotmail.fr>
2023-10-12 13:32:14 +0100

    first copy

---
+++ duoproject.js
@@ -0,0 +1,12 @@
+var sound = document.getElementById('sound');
+console.log(sound);
+
+function playsound(){
+  sound.play();
+}
+
+
+function startgame(){
+  window.location = 'startGame.html';
+}
+
